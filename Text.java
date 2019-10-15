b28489d32f597ed0946976832bb69622cf0ac8c4
965023780 <51905140+965023780@users.noreply.github.com>
2019-10-15 12:47:32 +0800

    Update Text.java

--- Text.java
+++ Text.java
@@ -1,44 +1,44 @@
 public class Text {
-    //测试
+    //TextDemo
     public static void main(String[] agrs) {
         mutable_array.MutableStringArray mutableStringArray = new mutable_array.MutableStringArray();
         String[] StringArray = {"A", "B", "C"};
         String ch = "A";
         int length;
         Boolean flag;
-        //储存A1——A9
+        //Add A1——A9 at the end
         for (int i = 0; i < 10; i++) {
             mutableStringArray.add(ch + i);
         }
         mutableStringArray.putOut();
-        //储存StringArray数组
+        //Add elements in StringArray
         mutableStringArray.add(StringArray);
         mutableStringArray.putOut();
-        //在data[5]中插入"A"
+        //Add String "A" at data[5]"
         mutableStringArray.addAt("A", 5);
         mutableStringArray.putOut();
-        //数组长度和已储存元素个数
+        //Print the length of array(length) and the number of elements(size)
         length = mutableStringArray.getLength();
         System.out.println("length=" + length);
         System.out.println("size=" + mutableStringArray.size);
-        //删除最后一个元素
+        //Delete the last data
         mutableStringArray.deleteLast();
         mutableStringArray.putOut();
-        //删除第五个元素
+        //Delete the data at data[5]
         mutableStringArray.deleteAt(5);
         mutableStringArray.putOut();
-        //将data[3]中元素更替为"d"
+        //Change the data at data[5] for String "d"
         mutableStringArray.changeAt("d", 3);
         mutableStringArray.putOut();
-        //检索数组中是否有A1
+        //Determine whether String "A1" is in the array
         flag = mutableStringArray.contains("A1");
         System.out.println(flag);
-        //检索数组中是否有A2
+        //Determine whether String "A10" is in the array
         flag = mutableStringArray.contains("A10");
         System.out.println(flag);
-        //输出data[3]中的元素
+        //Get and print the data at data[3]
         System.out.println(mutableStringArray.get(3));
-        //清空数组元素
+        //Clear the mutablearray
         mutableStringArray.clear();
         mutableStringArray.putOut();
     }
